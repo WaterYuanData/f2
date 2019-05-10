cb21abb5c8349312af8601b5f571b314a08bbf5d
WaterYuan <water.yuan.data@gmail.com>
2019-05-10 09:51:06 +0800

    原子类

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # secondRepository
 第二个仓库
+
+Android并发编程 原子类与并发容器
+https://blog.csdn.net/u010340035/article/details/85338991
--- testthread/src/main/java/com/example/testthread/MainActivity.java
+++ testthread/src/main/java/com/example/testthread/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.testthread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
+
+    private static final String TAG = "MainActivity";
+    private static AtomicLong sAtomicLong = new AtomicLong(10000L);
+
+    private void test() {
+
+//这里开了1000个线程对sCount并发操作
+        for (int i = 0; i < 1000; i++) {
+            Log.e(TAG, "run: --" + i);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    sAtomicLong.decrementAndGet();
+                    Log.i(TAG, "run: " + sAtomicLong.toString());
+                }
+            }).start();
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sAtomicLong);
     }
 }
